152a5f7cbaf9048908266afa2dddc0e18ce0592e
Atharva <atharva03kulkarni@gmail.com>
2020-11-21 09:18:28 +0530

    Shopping screen added

--- app/src/main/java/com/example/needforspeed/shoppingScreen.java
+++ app/src/main/java/com/example/needforspeed/shoppingScreen.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class shoppingScreen extends AppCompatActivity {
 
@@ -13,5 +15,13 @@ public class shoppingScreen extends AppCompatActivity {
         setContentView(R.layout.activity_shopping_screen);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+    }
+
+    public void sellingScreen(View view) {                                  //onClick = sellingScreen
+        Log.i("Button", "Sell button clicked");
+    }
+
+    public void buyingScreen(View view) {                                   //onClick = buyingScreen
+        Log.i("Button", "Buy button clicked");
     }
 }
