e4abd37e9c49df7815a41a183cefc303bfc75f6c
Soham Pardeshi <Pardeshi.Soham2@gmail.com>
2020-11-21 20:17:36 +0530

    Revert "Shopping screen added"
    This reverts commit d0ce23b2e0ef787fef870fc790ca083d4b9d8c7f.

--- app/src/main/java/com/example/needforspeed/shoppingScreen.java
+++ app/src/main/java/com/example/needforspeed/shoppingScreen.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 public class shoppingScreen extends AppCompatActivity {
 
@@ -15,13 +13,5 @@ public class shoppingScreen extends AppCompatActivity {
         setContentView(R.layout.activity_shopping_screen);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-    }
-
-    public void sellingScreen(View view) {                                  //onClick = sellingScreen
-        Log.i("Button", "Sell button clicked");
-    }
-
-    public void buyingScreen(View view) {                                   //onClick = buyingScreen
-        Log.i("Button", "Buy button clicked");
     }
 }
