971f315caa2b6318c3bbeae32297a0233ab2b493
Atharva <atharva03kulkarni@gmail.com>
2020-12-08 12:09:41 +0530

    working

---
+++ app/src/main/java/com/example/needforspeed/RetrievingValues.java
@@ -0,0 +1,22 @@
+package com.example.needforspeed;
+
+public class RetrievingValues {
+
+    private String value;
+
+    public RetrievingValues() {
+
+    }
+
+    public RetrievingValues(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
